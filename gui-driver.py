61b97999b9182fb4a78403c94aa9c01a35157a4a
Bhargav Joshi <bhargavjoshi@Bhargavs-MacBook-Pro.local>
2021-11-02 00:59:56 -0500

    GUI skeleton...COMPLETE

--- gui-driver.py
+++ gui-driver.py
@@ -1,59 +1,199 @@
-import Tkinter as tk
+from tkinter import *
 
-class Page(tk.Frame):
-    def __init__(self, *args, **kwargs):
-        tk.Frame.__init__(self, *args, **kwargs)
-    def show(self):
-        self.lift()
+class MainView(Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Insane Project")
+        self.geometry("1600x900")
+        
+        # Background Image
+        self.bg_img = PhotoImage(file="data/gui/images/background.png")
+        
+         # Create workspace
+        self.canvas = Canvas(self, width=1600, height=900)
+        self.canvas.pack()
 
-class Page1(Page):
-   def __init__(self, *args, **kwargs):
-       Page.__init__(self, *args, **kwargs)
-       label = tk.Label(self, text="This is page 1")
-       label.pack(side="top", fill="both", expand=True)
+        mbar = Menu(self)
+        self.config(menu=mbar)
 
-class Page2(Page):
-   def __init__(self, *args, **kwargs):
-       Page.__init__(self, *args, **kwargs)
-       label = tk.Label(self, text="This is page 2")
-       label.pack(side="top", fill="both", expand=True)
+        # Create menu items
+        file_menu = Menu(mbar)
+        help_menu = Menu(mbar)
 
-class Page3(Page):
-   def __init__(self, *args, **kwargs):
-       Page.__init__(self, *args, **kwargs)
-       label = tk.Label(self, text="This is page 3")
-       label.pack(side="top", fill="both", expand=True)
+        # Add menu parameters
+        file_menu.add_command(label='run', command=self.exit_command)
+        file_menu.add_command(label='exit', command=self.exit_command)
+        help_menu.add_command(label='Launch helper', command=self.exit_command)
 
+        # Add menus to the menubar
+        mbar.add_cascade(label="File", menu=file_menu)
+        mbar.add_cascade(label='Help', menu=help_menu)
 
-class MainView(tk.Frame):
-    def __init__(self, *args, **kwargs):
-        tk.Frame.__init__(self, *args, **kwargs)
-        p1 = Page1(self)
-        p2 = Page2(self)
-        p3 = Page3(self)
+        # Dropdown menu
+        self.rob_options = [
+            "Backdoor Attacks",
+            "Data Poisoning Attacks",
+            "Model Poisoning Attacks",
+            "Byzantine Attacks",
+            "Free-rider Attacks",
+            "Inference Attacks",
+            "Byzantine Robustness",
+            "Sybil Robustness",
+            "Certified Robustness",
+        ]
+        self.rob_clicked = StringVar(self)
+        self.rob_clicked.set( "Select" )
+        self.robustness_drop = OptionMenu(self , self.rob_clicked , *self.rob_options, command=self.rob_option_changed)
 
-        buttonframe = tk.Frame(self)
-        container = tk.Frame(self)
-        buttonframe.pack(side="top", fill="x", expand=False)
-        container.pack(side="top", fill="both", expand=True)
+        self.eff_options = [
+            "Effecitveness",
+        ]
+        self.eff_clicked = StringVar(self)
+        self.eff_clicked.set( "Select" )
+        self.eff_drop = OptionMenu(self , self.eff_clicked , *self.eff_options, command=self.eff_option_changed)
 
-        p1.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
-        p2.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
-        p3.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        self.priv_options = [
+            "Privacy",
+        ]
+        self.priv_clicked = StringVar(self)
+        self.priv_clicked.set( "Select" )
+        self.priv_drop = OptionMenu(self , self.priv_clicked , *self.priv_options, command=self.priv_option_changed)
 
-        b1 = tk.Button(buttonframe, text="Page 1", command=p1.show)
-        b2 = tk.Button(buttonframe, text="Page 2", command=p2.show)
-        b3 = tk.Button(buttonframe, text="Page 3", command=p3.show)
+        self.fair_options = [
+            "Fairness",
+        ]
+        self.fair_clicked = StringVar(self)
+        self.fair_clicked.set( "Select" )
+        self.fair_drop = OptionMenu(self , self.fair_clicked , *self.fair_options, command=self.fair_option_changed)
 
-        b1.pack(side="left")
-        b2.pack(side="left")
-        b3.pack(side="left")
+        self.generate_template()
 
-        p1.show()
+    def eff_option_changed(self, *args):
+        self.eff_option = self.eff_clicked.get()
+        if self.eff_option == self.eff_options[0]:
+            self.e_page_1()
+
+    def priv_option_changed(self, *args):
+        self.priv_option = self.priv_clicked.get()
+        if self.priv_option == self.priv_options[0]:
+            self.p_page_1()
+
+    def rob_option_changed(self, *args):
+        self.rob_option = self.rob_clicked.get()
+        if self.rob_option == self.rob_options[0]:
+            self.r_page_1()
+        elif self.rob_option == self.rob_options[1]:
+            self.r_page_2()
+        elif self.rob_option == self.rob_options[2]:
+            self.r_page_3()
+        elif self.rob_option == self.rob_options[3]:
+            self.r_page_4()
+        elif self.rob_option == self.rob_options[4]:
+            self.r_page_5()
+        elif self.rob_option == self.rob_options[5]:
+            self.r_page_6()
+        elif self.rob_option == self.rob_options[6]:
+            self.r_page_7()
+        elif self.rob_option == self.rob_options[7]:
+            self.r_page_8()
+        elif self.rob_option == self.rob_options[8]:
+            self.r_page_9()
+    
+    def fair_option_changed(self, *args):
+        self.fair_option = self.fair_clicked.get()
+        if self.fair_option == self.fair_options[0]:
+            self.f_page_1()
+    
+    def e_page_1(self):
+        self.clean()
+        self.canvas.create_text(800, 450, text="Effectiveness", font=('Helvatica', 24), fill='Gray', tags='del')
+        pass
+    
+    def p_page_1(self):
+        self.clean()
+        self.canvas.create_text(800, 450, text="Privacy", font=('Helvatica', 24), fill='Gray', tags='del')
+        pass
+
+    def r_page_1(self):
+        self.clean()
+        self.canvas.create_text(800, 450, text="Backdoor Attacks", font=('Helvatica', 24), fill='Gray', tags='del')
+        pass
+    
+    def r_page_2(self):
+        self.clean()
+        self.canvas.create_text(800, 450, text="Data Poisoning Attacks", font=('Helvatica', 24), fill='Gray', tags='del')
+        pass
+    
+    def r_page_3(self):
+        self.clean()
+        self.canvas.create_text(800, 450, text="Model Poisoning Attacks", font=('Helvatica', 24), fill='Gray', tags = 'del')
+        pass
+    
+    def r_page_4(self):
+        self.clean()
+        self.canvas.create_text(800, 450, text="Byzantine Attacks", font=('Helvatica', 24), fill='Gray', tags = 'del')
+        pass
+    
+    def r_page_5(self):
+        self.clean()
+        self.canvas.create_text(800, 450, text="Free-rider Attacks", font=('Helvatica', 24), fill='Gray', tags = 'del')
+        pass
+
+    def r_page_6(self):
+        self.clean()
+        self.canvas.create_text(800, 450, text="Inference Attacks", font=('Helvatica', 24), fill='Gray', tags = 'del')
+        pass
+    
+    def r_page_7(self):
+        self.clean()
+        self.canvas.create_text(800, 450, text="Byzantine Robustness", font=('Helvatica', 24), fill='Gray', tags = 'del')
+        pass
+    
+    def r_page_8(self):
+        self.clean()
+        self.canvas.create_text(800, 450, text="Sybil Robustness", font=('Helvatica', 24), fill='Gray', tags = 'del')
+        pass
+    
+    def r_page_9(self):
+        self.clean()
+        self.canvas.create_text(800, 450, text="Certified Robustness", font=('Helvatica', 24), fill='Gray', tags = 'del')
+        pass
+    
+    def f_page_1(self):
+        self.clean()
+        self.canvas.create_text(800, 450, text="Fairness", font=('Helvatica', 24), fill='Gray', tags='del')
+        pass
+
+    def exit_command(self):
+        self.quit()
+    
+    def helper():
+        pass
+    
+    def generate_template(self):
+        self.canvas.create_image(-786, 0, image=self.bg_img, anchor=NW)
+        
+        self.canvas.create_text(195, 135, text="Effectiveness", font=('Helvatica', 40), fill='Gray')
+        self.canvas.create_window(80, 160, anchor=NW, window=self.eff_drop)
+
+        self.canvas.create_text(148, 330, text="Privacy ", font=('Helvatica', 40), fill='Gray')
+        self.canvas.create_window(80, 360, anchor=NW, window=self.priv_drop)
+
+        self.canvas.create_text(183, 530, text="Robustness", font=('Helvatica', 40), fill='Gray')
+        self.canvas.create_window(80, 560, anchor=NW, window=self.robustness_drop)
+
+        self.canvas.create_text(152, 730, text="Fairness", font=('Helvatica', 40), fill='Gray')
+        self.canvas.create_window(80, 760, anchor=NW, window=self.fair_drop)
+
+        self.canvas.create_text(800, 450, text="Welcome", font=('Helvatica', 24), fill='Gray', tags = 'del')
+        
+    def clean(self):
+        self.canvas.delete("del")
+    
+    def reset(self):
+        self.clean()
+        self.generate_template()
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    main = MainView(root)
-    main.pack(side="top", fill="both", expand=True)
-    root.wm_geometry("400x400")
-    root.mainloop()
+    app = MainView()
+    app.mainloop()
